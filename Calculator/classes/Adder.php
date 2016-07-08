93f5a920c1ecd07c5e9c36caa0f3af2c8be57ac3
Sirus <sirusdas@gmail.com>
2016-07-08 22:39:55 +0530

    Do it

--- Calculator/classes/Adder.php
+++ Calculator/classes/Adder.php
@@ -1,11 +1,12 @@
 <?php
 class Adder implements OperatorInterface{
-	public $isFirst = false;
+	public $isFirst = false; //skip for now
 	
 	public function run($number, $result){
 		return $result + $number;
 	}
 	
+	//skip this for now
 	public function setFirst($isFirst){
 		$this->isFirst = $isFirst;
 	}
--- Calculator/index.php
+++ Calculator/index.php
@@ -2,8 +2,8 @@
 require 'init.php';
 
 $c = new Calculator();
-/* $c->setOperation(new Adder());
-$c->calculate(5, 5); */
+$c->setOperation(new Adder());
+$c->calculate(5, 5);
 
 /* $c->setOperation(new Multiplier());
 $c->setFirst(true);
@@ -13,10 +13,10 @@ $c->setOperation(new Subtractor());
 //$c->setFirst(true);
 $c->calculate(5); */
 
-$c->setOperation(new Divider());
+/* $c->setOperation(new Divider());
 $c->setFirst(true);
 $c->calculate(15, 5);
-
+ */
 $c->getResult();
 
 ?>
