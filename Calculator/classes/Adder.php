8b211ce92befc595a44506d0e8ed2f2018e6aab4
Sirus <sirusdas@gmail.com>
2016-07-09 00:41:41 +0530

    back to same

--- Calculator/classes/Adder.php
+++ Calculator/classes/Adder.php
@@ -1,14 +1,13 @@
 <?php
 class Adder implements OperatorInterface{
-/* 	public $isFirst = false; //skip for now */
+	public $isFirst = false;
 	
 	public function run($number, $result){
 		return $result + $number;
 	}
 	
-/* 	//skip this for now
 	public function setFirst($isFirst){
 		$this->isFirst = $isFirst;
-	} */
+	}
 	
 }
--- Calculator/classes/Calculator.php
+++ Calculator/classes/Calculator.php
@@ -12,13 +12,13 @@ protected  $operation;
 	public function calculate(){
 		
 		foreach (func_get_args() as $number){
-		/* 	if($this->operation->isFirst == false){ */
+			if($this->operation->isFirst == false){
 				$this->result = $this->operation->run($number, $this->result); //result + number
-/* 			}else{
+			}else{
 				$this->operation->setFirst(true);
 				$this->result = $this->operation->run($this->result, $number ); //number + result
 				$this->operation->setFirst(false);
-			} */
+			}
 		}
 		
 /* 		if($this->operation->isFirst == true){
@@ -31,9 +31,9 @@ protected  $operation;
 		echo 'Result = ' . $this->result;
 	}
 	
-/* 	public function setFirst($isFirst){
+	public function setFirst($isFirst){
 		$this->operation->isFirst = $isFirst;
 	}
-	 */
+	
 }
 ?>
